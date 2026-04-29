911ea9228681f527459b0fd2e204043d9b7902b3
Vyshakh Vijayan <vyshakhvponnachat@gmail.com>
2026-04-29 14:42:36 +0530

    chore: backup messy state before rebuild

--- database/migrations/004_backfill_onboarding_flag.sql
+++
@@ -1,8 +0,0 @@
--- 004_backfill_onboarding_flag.sql
--- Backfill onboarding_complete for all existing profiles with real data
-
-UPDATE "Store Profiles"
-SET onboarding_complete = true
-WHERE store_name IS NOT NULL
-  AND store_name != ''
-  AND (onboarding_complete IS NULL OR onboarding_complete = false);
